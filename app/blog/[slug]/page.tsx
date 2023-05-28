aabe848778746a4e3fb7f4a5f3158a609298b81a
YOOUMUU <yooumuu@gmail.com>
2023-05-28 19:46:33 +0800

    Static generate

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+export const revalidate = 600;
 
 interface Post {
   title: string;
@@ -9,6 +9,16 @@ interface Post {
 interface Props {
   params: { slug: string };
 }
+
+// Tell next.js which pages to static generate(pages that not change often)
+// Add revalidate to tell next.js how often to re-generate the page
+export const generateStaticParams = async () => {
+  const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+    (res) => res.json()
+  );
+
+  return posts.map((post) => ({ slug: post.slug }));
+};
 
 const BlogPostPage = async ({ params }: Props) => {
   const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
