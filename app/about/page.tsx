b0039fdedf7ebfa39d140189a54da215db387cc1
YOOUMUU <yooumuu@gmail.com>
2023-05-29 19:00:55 +0800

    Protect auth route (about page)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,25 @@
 export const dynamic = 'force-static';
 
 import { Metadata } from 'next';
+import { getServerSession } from 'next-auth/next';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'About me',
   description: 'A person who loves to learn new things',
 };
 
-const AboutPage = () => {
+const AboutPage = async () => {
+  const session = await getServerSession();
+
+  // if (!session) {
+  //   return <p>You must be signed in...</p>
+  // }
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
   return (
     <div className="container">
       <h1 className="title">About me</h1>
