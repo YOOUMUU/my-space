d8a0ba8d09df752b738c7698018858228818e619
YOOUMUU <yooumuu@gmail.com>
2023-06-01 12:32:04 +0800

    Follow server prepared

--- app/dashboard/ProfileForm.tsx
+++ app/dashboard/ProfileForm.tsx
@@ -8,7 +8,7 @@ const ProfileForm = ({ user }: any) => {
 
     const body = {
       name: formData.get('name'),
-      email: formData.get('email'),
+      bio: formData.get('bio'),
       image: formData.get('image'),
     };
 
@@ -41,15 +41,14 @@ const ProfileForm = ({ user }: any) => {
         </div>
         <div className="mb-5">
           <label
-            htmlFor="email"
+            htmlFor="bio"
             className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-md font-medium text-gray-700 mb-1"
           >
-            Email
+            Bio
           </label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={user?.email ?? ''}
+          <textarea
+            name="bio"
+            defaultValue={user?.bio ?? ''}
             className="mt-1 px-3 py-2 bg-white border shadow-sm border-gray-300 placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded sm:text-md focus:ring-1"
           />
         </div>
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -14,13 +14,13 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function UserProfile({ params }: Props) {
   const user = await prisma.user.findUnique({ where: { id: params.id } });
-  const { name, email, image } = user ?? {};
+  const { name, bio, image } = user ?? {};
 
   return (
     <div className="container">
       <h1 className="title font-bold">{name}</h1>
       <img src={image ?? '/mememan.webp'} alt={`${name}'s profile picture`} />
-      <p className="mt-5 text-md font-medium">{`Email: ${email}`}</p>
+      <p className="mt-5 text-md font-medium">{bio}</p>
     </div>
   );
 }
